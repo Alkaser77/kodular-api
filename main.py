b6fd2a03d0a4f6016d583f401237f4abd7bbfef9
Alkaser77 <abdelazizali778@gmail.com>
2026-08-25 10:03:32 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+import time
+
+app = Flask(__name__)
+GOOGLE_DRIVE_LINK = "https://drive.google.com/uc?export=download&id=1ABCxyz123"
+
+@app.route('/')
+def home():
+    return "API is Running"
+
+@app.route('/check', methods=['POST'])
+def check():
+    data = request.get_json()
+    return jsonify({"status":"active","key":"50523a49","expire":int(time.time()*1000)+86400000,"link":GOOGLE_DRIVE_LINK})
+
+if __name__ == '__main__':
+    app.run()
